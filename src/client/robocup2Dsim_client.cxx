ceafe4e900ab1542473565065e1e75e60ba8d417
Kean H Lau <khklau@users.noreply.github.com>
2015-09-08 22:25:19 +1000

    * refactored the command line argument parsing code * the bot argument list offset is now calculated rather than hard coded * configured GLog to log to standard error and set the minimum log level

--- src/client/robocup2Dsim_client.cxx
+++ src/client/robocup2Dsim_client.cxx
@@ -14,25 +14,44 @@ namespace tpp = turbo::process::posix;
 
 struct config
 {
-    config() : port(0) { }
+    config() : port(0), bot_arg_count(0), bot_arg_offset(0) { }
     std::string address;
     int port;
     std::string bot_path;
-    std::vector<std::string> bot_arg_list;
+    unsigned short bot_arg_count;
+    unsigned short bot_arg_offset;
 };
 
-int main(int argc, char* argv[])
+void parse_cmd_args(int argc, char* argv[], config& conf)
 {
-    config conf;
     kj::TopLevelProcessContext context(argv[0]);
     kj::MainFunc parse = kj::MainBuilder(
 	    context,
 	    "Robocup 2D Simulation Client v0.0",
 	    "Launches bot and connects to the server")
-	.expectArg("address", [&] (kj::StringPtr arg) { conf.address = arg.cStr(); return true; })
-	.expectArg("port", [&] (kj::StringPtr arg) { conf.port = atoi(arg.cStr()); return true; })
-	.expectArg("bot_path", [&] (kj::StringPtr arg) { conf.bot_path = arg.cStr(); return true; })
-	.expectZeroOrMoreArgs("bot_arg", [&] (kj::StringPtr arg) { conf.bot_arg_list.push_back(arg.cStr()); return true; })
+	.expectArg("address", [&] (kj::StringPtr arg)
+	{
+	    conf.address = arg.cStr();
+	    ++conf.bot_arg_offset;
+	    return true;
+	})
+	.expectArg("port", [&] (kj::StringPtr arg)
+	{
+	    conf.port = atoi(arg.cStr());
+	    ++conf.bot_arg_offset;
+	    return true;
+	})
+	.expectArg("bot_path", [&] (kj::StringPtr arg)
+	{
+	    conf.bot_path = arg.cStr();
+	    ++conf.bot_arg_offset;
+	    return true;
+	})
+	.expectZeroOrMoreArgs("bot_arg", [&] (kj::StringPtr)
+	{
+	    ++conf.bot_arg_count;
+	    return true;
+	})
 	.build();
     KJ_STACK_ARRAY(kj::StringPtr, params, argc - 1, 8, 32);
     for (int iter = 1; iter < argc; ++iter)
@@ -40,11 +59,16 @@ int main(int argc, char* argv[])
 	params[iter - 1] = argv[iter];
     }
     parse(argv[0], params);
-    if (kj::_::Debug::shouldLog(kj::_::Debug::Severity::INFO))
-    {
-    }
-    tpp::child&& bot = tpp::spawn(conf.bot_path.c_str(), &argv[4], {}, 2 << 16);
+}
+
+int main(int argc, char* argv[])
+{
+    config conf;
+    parse_cmd_args(argc, argv, conf);
+    tpp::child&& bot = tpp::spawn(conf.bot_path.c_str(), &argv[conf.bot_arg_offset], {}, 2 << 16);
     google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = true;
+    FLAGS_minloglevel = (kj::_::Debug::shouldLog(kj::_::Debug::Severity::INFO)) ? 0 : 1;
     google::InstallFailureSignalHandler();
     return 0;
 }
