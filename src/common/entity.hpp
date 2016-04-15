07a191612b957cd95b3f96986ee33cb616792fa1
Kean H Lau <khklau@users.noreply.github.com>
2016-04-16 00:50:30 +1000

    adding in entity header which declares types that are not covered in the Capn Proto file

---
+++ src/common/entity.hpp
@@ -0,0 +1,27 @@
+#ifndef ROBOCUP2DSIM_COMMON_ENTITY_HPP
+#define ROBOCUP2DSIM_COMMON_ENTITY_HPP
+
+#include <robocup2Dsim/common/entity.capnp.h>
+#include <cstdint>
+
+namespace robocup2Dsim {
+namespace common {
+namespace entity {
+
+enum class team_id : uint8_t
+{
+    alpha,
+    beta
+};
+
+struct player_id
+{
+    UniformNumber uniform;
+    team_id team;
+};
+
+} // namespace entity
+} // namespace common
+} // namespace robocup2Dsim
+
+#endif
