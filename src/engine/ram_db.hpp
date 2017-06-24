faf087f0fc3f90fa56dc3a5aedc1e0fc23b0ea5b
Kean H Lau <khklau@users.noreply.github.com>
2017-06-24 15:57:20 +1000

    * table now contains a member std::array containing the column names * implemented table::emplace * added make_unique_table & delete_unique_table

--- src/engine/ram_db.hpp
+++ src/engine/ram_db.hpp
@@ -1,26 +1,52 @@
 #ifndef ROBOCUP2DSIM_ENGINE_RAM_DB_HPP
 #define ROBOCUP2DSIM_ENGINE_RAM_DB_HPP
 
+#include <array>
+#include <memory>
 #include <tuple>
 #include <unordered_map>
+#include <robocup2Dsim/engine/primitives.hpp>
 #include <turbo/memory/pool.hpp>
+#include <turbo/toolset/attribute.hpp>
 
 namespace robocup2Dsim {
 namespace engine {
 
-template <class key_t, class value_t>
-class table
+enum class emplace_result
+{
+    success,
+    key_already_exists
+};
+
+template <class key_t, class... values_t>
+class TURBO_SYMBOL_DECL table
 {
 public:
     typedef key_t key_type;
-    typedef value_t value_type;
-    table();
-    table(std::size_t initial_size);
-private:
-    typedef std::tuple<key_t, value_t> tuple_type;
+    static constexpr std::size_t total_columns()
+    {
+	// the extra 1 is the key column
+	return 1U + sizeof...(values_t);
+    }
     turbo::memory::block_list data_;
-    std::unordered_map<key_t, value_t> index_;
+    template <class... args_t>
+    table(std::size_t initial_size, const char* key_name, args_t&&... args);
+    template <class... args_t>
+    emplace_result emplace(key_type key, args_t&&... args);
+private:
+    typedef std::tuple<key_t, values_t...> tuple_type;
+    std::array<fixed_cstring_32, total_columns()> column_names_;
+    std::unordered_map<key_t, tuple_type*> index_;
 };
+
+struct TURBO_SYMBOL_DECL some_table;
+
+typedef std::unique_ptr<some_table, void (*)(some_table*)> unique_table_ptr;
+
+template <class key_t, class... values_t>
+unique_table_ptr make_unique_table(table<key_t, values_t...>* table);
+
+typedef table<robocup2Dsim::engine::key_16, robocup2Dsim::engine::fixed_cstring_32, unique_table_ptr> catalog;
 
 } // namespace engine
 } // namespace robocup2Dsim
--- src/engine/ram_db.hxx
+++ src/engine/ram_db.hxx
@@ -1,21 +1,65 @@
 #ifndef ROBOCUP2DSIM_ENGINE_RAM_DB_HXX
 #define ROBOCUP2DSIM_ENGINE_RAM_DB_HXX
 
+#include <robocup2Dsim/engine/primitives.hxx>
+#include <turbo/memory/block.hpp>
+#include <turbo/toolset/extension.hpp>
+
+namespace {
+
+using namespace robocup2Dsim::engine;
+
+template <class key_t, class... values_t>
+void delete_unique_table(some_table* something)
+{
+     auto actual_table = static_cast<table<key_t, values_t...>*>(static_cast<void*>(something));
+     delete actual_table;
+}
+
+} // anonymous namespace
+
 namespace robocup2Dsim {
 namespace engine {
 
-template <class k, class v>
-table<k, v>::table()
-    :
-	table(32U)
-{ }
-
-template <class k, class v>
-table<k, v>::table(std::size_t initial_size)
+template <class k, class... vs>
+template <class... args_t>
+table<k, vs...>::table(std::size_t initial_size, const char* key_name, args_t&&... args)
     :
 	data_(sizeof(tuple_type), initial_size),
+	column_names_{{ key_name, args... }},
 	index_()
-{ }
+{
+    static_assert(sizeof...(vs) == sizeof...(args), "The number of column name arguments "
+	    "does not match the number of columns in the table");
+}
+
+template <class k, class... vs>
+template <class... args_t>
+emplace_result table<k, vs...>::emplace(key_type key, args_t&&... args)
+{
+    if (index_.count(key) > 0)
+    {
+	return emplace_result::key_already_exists;
+    }
+    tuple_type* entry = static_cast<tuple_type*>(data_.allocate());
+    if (TURBO_UNLIKELY(entry == nullptr))
+    {
+	throw turbo::memory::out_of_memory_error("Insufficient free memory to allocate the requested table row");
+    }
+    else
+    {
+	new (entry) tuple_type(key, std::forward<args_t>(args)...);
+	index_.emplace(key, entry);
+	return emplace_result::success;
+    }
+}
+
+template <class key_t, class... values_t>
+unique_table_ptr make_unique_table(table<key_t, values_t...>* table_ptr)
+{
+    return std::move(unique_table_ptr(static_cast<some_table*>(static_cast<void*>(table_ptr)),
+	    delete_unique_table<key_t, values_t...>));
+}
 
 } // namespace engine
 } // namespace robocup2Dsim
