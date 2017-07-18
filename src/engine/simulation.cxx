7a5fc55dc2512f76a47a9af6c8c47bcb083cf8de
Kean H Lau <khklau@users.noreply.github.com>
2017-07-18 18:59:36 +1000

    the physics system now registers itself

--- src/engine/simulation.cxx
+++ src/engine/simulation.cxx
@@ -2,6 +2,9 @@
 #include <Box2D/Common/b2Math.h>
 #include <SnapBox2D/snapshot.hpp>
 #include <SnapBox2D/storage.hpp>
+#include "ecs_db.hpp"
+#include "ecs_db.hxx"
+#include "ram_db.hxx"
 
 namespace robocup2Dsim {
 namespace engine {
@@ -10,6 +13,14 @@ simulation::simulation()
     :
 	world_(b2Vec2(0, 0))
 { }
+
+void register_system(ecs_db& db, std::unique_ptr<simulation> sim)
+{
+    ecs_db::system_table_type& sys_table = db.access<ecs_db::system_table_type>(table_id::system_registry);
+    physics_table_type* table = new physics_table_type(1U, "task_id", "physics_instance", "instance_name");
+    table->emplace(0U, std::move(sim), "active_box2d");
+    sys_table.emplace(system_id::physics, make_unique_table(table), "physics");
+}
 
 } // namespace engine
 } // robocup2Dsim
--- src/engine/simulation.hpp
+++ src/engine/simulation.hpp
@@ -1,18 +1,27 @@
 #ifndef ROBOCUP2DSIM_ENGINE_SIMULATION_HPP
 #define ROBOCUP2DSIM_ENGINE_SIMULATION_HPP
 
+#include <memory>
 #include <Box2D/Dynamics/b2World.h>
+#include <turbo/toolset/attribute.hpp>
+#include "ram_db.hpp"
 
 namespace robocup2Dsim {
 namespace engine {
 
-class simulation
+class TURBO_SYMBOL_DECL simulation
 {
 public:
     simulation();
 private:
     b2World world_;
 };
+
+typedef table<primitives::key_16, std::unique_ptr<simulation>, primitives::fixed_cstring_32> physics_table_type;
+
+class ecs_db;
+
+void register_system(ecs_db& db, std::unique_ptr<simulation> sim);
 
 } // namespace engine
 } // namespace robobup2Dsim
