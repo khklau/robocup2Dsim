e4bfe2cc61f5925377553853e60b3e6262cef838
Kean H Lau <khklau@users.noreply.github.com>
2017-07-19 18:06:13 +1000

    adding initial dynamics module

---
+++ src/engine/dynamics.hpp
@@ -0,0 +1,14 @@
+#ifndef ROBOCUP2DSIM_ENGINE_PRESENCE_HPP
+#define ROBOCUP2DSIM_ENGINE_PRESENCE_HPP
+
+#include <Box2D/Dynamics/b2Body.h>
+
+namespace robocup2Dsim {
+namespace engine {
+namespace dynamics {
+
+typedef b2Body body;
+
+} // namespace dynamics
+} // namespace engine
+} // namespace robocup2Dsim
