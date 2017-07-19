3779ec220a3fe59932cf416249691308e8d6efc8
Kean H Lau <khklau@users.noreply.github.com>
2017-07-19 18:21:02 +1000

    replacing all references to simulation with physics

--- src/engine/physics.cxx
+++ src/engine/physics.cxx
@@ -1,4 +1,4 @@
-#include "simulation.hpp"
+#include "physics.hpp"
 #include <Box2D/Common/b2Math.h>
 #include <SnapBox2D/snapshot.hpp>
 #include <SnapBox2D/storage.hpp>
@@ -9,16 +9,16 @@
 namespace robocup2Dsim {
 namespace engine {
 
-simulation::simulation()
+physics::physics()
     :
 	world_(b2Vec2(0, 0))
 { }
 
-void register_system(ecs_db& db, std::unique_ptr<simulation> sim)
+void register_system(ecs_db& db, std::unique_ptr<physics> phys)
 {
     ecs_db::system_table_type& sys_table = db.access<ecs_db::system_table_type>(table_id::system_registry);
     physics_table_type* table = new physics_table_type(1U, "task_id", "physics_instance", "instance_name");
-    table->emplace(0U, std::move(sim), "active_box2d");
+    table->emplace(0U, std::move(phys), "active_box2d");
     sys_table.emplace(system_id::physics, make_unique_table(table), "physics");
 }
 
--- src/engine/physics.hpp
+++ src/engine/physics.hpp
@@ -1,5 +1,5 @@
-#ifndef ROBOCUP2DSIM_ENGINE_SIMULATION_HPP
-#define ROBOCUP2DSIM_ENGINE_SIMULATION_HPP
+#ifndef ROBOCUP2DSIM_ENGINE_PHYSICS_HPP
+#define ROBOCUP2DSIM_ENGINE_PHYSICS_HPP
 
 #include <memory>
 #include <Box2D/Dynamics/b2World.h>
@@ -9,19 +9,19 @@
 namespace robocup2Dsim {
 namespace engine {
 
-class TURBO_SYMBOL_DECL simulation
+class TURBO_SYMBOL_DECL physics
 {
 public:
-    simulation();
+    physics();
 private:
     b2World world_;
 };
 
-typedef table<primitives::key_16, std::unique_ptr<simulation>, primitives::fixed_cstring_32> physics_table_type;
+typedef table<primitives::key_16, std::unique_ptr<physics>, primitives::fixed_cstring_32> physics_table_type;
 
 class ecs_db;
 
-void register_system(ecs_db& db, std::unique_ptr<simulation> sim);
+void register_system(ecs_db& db, std::unique_ptr<physics> phys);
 
 } // namespace engine
 } // namespace robobup2Dsim
