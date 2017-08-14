73a13345b76c2f6f3701c58150ebf20e3a28ca40
Kean H Lau <khklau@users.noreply.github.com>
2017-08-14 18:34:36 +1000

    instead of having a sub-table for each component, the ecs database now holds the components in a slab allocator

--- src/engine/ecs_db.cxx
+++ src/engine/ecs_db.cxx
@@ -4,14 +4,15 @@
 namespace robocup2Dsim {
 namespace engine {
 
-ecs_db::ecs_db()
+ecs_db::ecs_db(std::size_t contingency_capacity, const std::vector<turbo::memory::block_config>& allocator_config)
     :
+	component_allocator_(contingency_capacity, allocator_config),
 	catalog_(64U, "table_id", "table_name", "table_ptr")
 {
     catalog_.emplace(table_id::entity_registry, "entity_registry", make_unique_table(
 	    new entity_table_type(32U, "entity_id", "entity_name")));
     catalog_.emplace(table_id::component_registry, "component_registry", make_unique_table(
-	    new component_table_type(64U, "component_id", "component_table_ptr", "component_name")));
+	    new component_table_type(64U, "component_id", "component_name")));
     catalog_.emplace(table_id::system_registry, "system_registry", make_unique_table(
 	    new system_table_type(8U, "system_id", "system_table_ptr", "system_name")));
     catalog_.emplace(table_id::entity_component_map, "entity_component_map", make_unique_table(
--- src/engine/ecs_db.hpp
+++ src/engine/ecs_db.hpp
@@ -1,8 +1,11 @@
 #ifndef ROBOCUP2DSIM_ENGINE_ECS_DB_HPP
 #define ROBOCUP2DSIM_ENGINE_ECS_DB_HPP
 
+#include <cstddef>
 #include <cstdint>
+#include <vector>
 #include <robocup2Dsim/engine/ram_db.hpp>
+#include <turbo/memory/slab_allocator.hpp>
 #include <turbo/toolset/attribute.hpp>
 
 namespace robocup2Dsim {
@@ -34,16 +37,21 @@ class TURBO_SYMBOL_DECL ecs_db
 {
 public:
     typedef table<primitives::key_16, primitives::fixed_cstring_32> entity_table_type;
-    typedef table<primitives::key_16, unique_table_ptr, primitives::fixed_cstring_32> component_table_type;
+    typedef table<primitives::key_16, primitives::fixed_cstring_32> component_table_type;
     typedef table<primitives::key_16, unique_table_ptr, primitives::fixed_cstring_32> system_table_type;
     typedef table<primitives::key_16, primitives::key_16, primitives::key_32> entity_component_table_type;
     typedef table<primitives::key_16, primitives::key_16, primitives::key_32> component_system_table_type;
-    ecs_db();
+    ecs_db(std::size_t contingency_capacity, const std::vector<turbo::memory::block_config>& allocator_config);
     template <class table_t>
     table_t& access(table_id::type id);
     template <class table_t>
     const table_t& access(table_id::type id) const;
+    inline turbo::memory::concurrent_sized_slab& component_allocator()
+    {
+	return component_allocator_;
+    }
 private:
+    turbo::memory::concurrent_sized_slab component_allocator_;
     robocup2Dsim::engine::catalog catalog_;
 };
 
