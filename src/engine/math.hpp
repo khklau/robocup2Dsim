8d270648b7eb92f58b7fb7cd82c0f2a4d497e95d
Kean H Lau <khklau@users.noreply.github.com>
2017-09-04 19:10:45 +1000

    adding initial engine math module

---
+++ src/engine/math.hpp
@@ -0,0 +1,16 @@
+#ifndef TURBO_ENGINE_MATH_HPP
+#define TURBO_ENGINE_MATH_HPP
+
+#include <array>
+
+namespace turbo {
+namespace engine {
+namespace math {
+
+typedef std::array<float, 2U> coord2d;
+
+} // namespace math
+} // namespace engine
+} // namespace turbo
+
+#endif
