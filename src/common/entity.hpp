240f954d96d7d9dd760680a39cb454ff0c41a2ef
Kean H Lau <khklau@users.noreply.github.com>
2017-10-11 18:22:11 +1100

    adding initial collision category and fixture name definitions

--- src/common/entity.hpp
+++ src/common/entity.hpp
@@ -8,6 +8,31 @@ namespace robocup2Dsim {
 namespace common {
 namespace entity {
 
+enum class collision_category : std::uint8_t
+{
+    ball,
+    goal_post,
+    goal_net,
+    goal_sensor,
+    player_torso,
+    player_foot,
+    player_head,
+    player_sensor,
+    field_marker,
+    field_sensor,
+    max = field_sensor
+};
+
+enum class fixture_name : std::uint16_t
+{
+    ball,
+    top_post,
+    bottom_post,
+    back_net,
+    top_net,
+    bottom_net
+};
+
 enum class team_id : uint8_t
 {
     alpha,
