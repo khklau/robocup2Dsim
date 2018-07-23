2f0783908f12eb9102f23404f9f9647fb8e2dc71
Kean H Lau <khklau@users.noreply.github.com>
2018-07-24 08:53:26 +1000

    adding initial registration processing code

--- src/referee/referee.cxx
+++ src/referee/referee.cxx
@@ -1,7 +1,9 @@
 #include "referee.hpp"
 
 namespace bmc = beam::message::capnproto;
+namespace rcc = robocup2Dsim::common::command;
 namespace rre = robocup2Dsim::referee;
+namespace rse = robocup2Dsim::server;
 namespace rsr = robocup2Dsim::srprotocol;
 
 namespace robocup2Dsim {
@@ -9,10 +11,33 @@ namespace referee {
 
 referee::referee(const config& conf)
     :
-	config_(conf)
+	config_(conf),
+	enrollment_(new rse::enrollment()),
+	roster_()
 { }
 
 void referee::process(std::queue<bmc::payload<rsr::RefInput>>& in, std::queue<bmc::payload<rsr::RefOutput>>& out)
+{
+    while (0U < in.size())
+    {
+	bmc::statement<rsr::RefInput> statement(std::move(in.front()));
+	in.pop();
+
+	rsr::RefInput::Reader input = statement.read();
+	switch (input.which())
+	{
+	    case rsr::RefInput::RULES:
+		break;
+	    case rsr::RefInput::REGISTRATION:
+		process(input.getRegistration(), out);
+		break;
+	}
+    }
+}
+
+void referee::process(
+	const rcc::Registration::Reader& registration,
+	std::queue<bmc::payload<rsr::RefOutput>>& out)
 {
 }
 
--- src/referee/referee.hpp
+++ src/referee/referee.hpp
@@ -4,8 +4,10 @@
 #include "config.hpp"
 
 #include <beam/message/capnproto.hh>
+#include <robocup2Dsim/common/command.capnp.h>
 #include <robocup2Dsim/srprotocol/protocol.capnp.h>
 #include <robocup2Dsim/srprotocol/protocol.hpp>
+#include <robocup2Dsim/server/roster.hpp>
 
 #include <queue>
 
@@ -19,8 +21,13 @@ public:
     void process(
 	    std::queue<beam::message::capnproto::payload<robocup2Dsim::srprotocol::RefInput>>& in,
 	    std::queue<beam::message::capnproto::payload<robocup2Dsim::srprotocol::RefOutput>>& out);
+    void process(
+	    const robocup2Dsim::common::command::Registration::Reader& registration,
+	    std::queue<beam::message::capnproto::payload<robocup2Dsim::srprotocol::RefOutput>>& out);
 private:
     config config_;
+    std::unique_ptr<robocup2Dsim::server::enrollment> enrollment_;
+    std::unique_ptr<robocup2Dsim::server::roster> roster_;
 };
 
 } // namespace referee
